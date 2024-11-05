2244476d68bf6d7bc61d787b84dfae4e0242e511
SofiiaKozlyk <ipz222_kso@student.ztu.edu.ua>
2024-11-05 10:42:17 +0200

    Updated StipePage

--- Hometask8/src/App.tsx
+++ Hometask8/src/App.tsx
@@ -1,33 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import { fetchExhibits } from './api/exhibitActions';
 import StipePage from './layouts/StipePage';
-import { ExhibitPropsI } from './api/exhibitActions';
 
 function App() {
-  const [exhibits, setExhibits] = useState<ExhibitPropsI[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchExhibits()
-      .then((response) => {
-        setExhibits(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching exhibits:", error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="App">
-      <StipePage exhibits={exhibits} />
+      <StipePage />
     </div>
   );
 }
--- Hometask8/src/layouts/StipePage.tsx
+++ Hometask8/src/layouts/StipePage.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ExhibitPropsI } from "../api/exhibitActions";
 import Post from "../components/Post";
+import { useRequest } from 'ahooks';
+import { fetchExhibits } from "../api/exhibitActions";
 
 
-const StipePage: React.FC<{exhibits: ExhibitPropsI[]}> = ({exhibits}) => {
-    console.log(exhibits);
+const StipePage: React.FC = () => {
+    const [exhibits, setExhibits] = useState<ExhibitPropsI[]>([]);
+    const { loading, error, run: fetchExhibitsAction } = useRequest(fetchExhibits, {
+        manual: true,
+        onSuccess: (data) => {
+            setExhibits(data.data);
+            console.log(data.data);
+        }
+    });
+
+    useEffect(() => {
+        fetchExhibitsAction();
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
 
     return (
         <>
-        {exhibits.map((exhibit) => (
+            {exhibits.map((exhibit) => (
                 <Post key={exhibit.id} exhibit={exhibit} />
             ))}
         </>
