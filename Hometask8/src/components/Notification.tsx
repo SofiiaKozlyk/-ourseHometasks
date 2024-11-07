a7d64f48588d8e7ac528ca8e23d3959115b17508
SofiiaKozlyk <ipz222_kso@student.ztu.edu.ua>
2024-11-07 19:05:42 +0200

    Code refactoring

--- Hometask8/src/components/Notification.tsx
+++ Hometask8/src/components/Notification.tsx
@@ -5,7 +5,8 @@ import Alert from '@mui/material/Alert';
 
 const SOCKET_SERVER_URL = 'http://ec2-13-49-67-34.eu-north-1.compute.amazonaws.com/notifications';
 
-const Notification = () => {
+
+const Notification = ({ children }: {children: React.ReactNode}) => {
     const [notifications, setNotifications] = useState<{ data: string; user: string } | null>(null);
     const [open, setOpen] = useState(false);
 
@@ -41,89 +42,9 @@ const Notification = () => {
                     {notifications ? `New post by ${notifications.user}: ${notifications.data}` : ""}
                 </Alert>
             </Snackbar>
+            {children}
         </>
     );
 };
 
 export default Notification;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useEffect, useState } from "react";
-// import { io } from "socket.io-client";
-
-// const SOCKET_SERVER_URL = 'http://ec2-13-49-67-34.eu-north-1.compute.amazonaws.com/notifications';
-
-// const Notification = ({children}: {children: React.ReactNode}) => {
-//     const [notifications, setNotifications] = useState<{ data: string; user: string }>({
-//         data: '',
-//         user: '',
-//     });
-
-//     useEffect(() => {
-//         const socket = io(SOCKET_SERVER_URL, {
-//             extraHeaders: {
-//                 Authorization: `Bearer `,
-//             }
-//         },
-//         );
-
-//         socket.on('newPost', (data) => {
-//             setNotifications({
-//                 data: data.message,
-//                 user: data.user,
-//             })
-//             console.log('Нове повідомлення: ', data.message, data.user);
-//         });
-
-//         return () => {
-//             socket.disconnect();
-//         };
-//     }, []);
-
-
-//     return (
-//         <>
-//         <div>
-//             {notifications.data}
-//         </div>
-//         {children}
-//         </>
-//     )
-
-// }
-
-// export default Notification;
